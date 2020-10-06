8a7919e408869a854a246164338d1598550d09cc
Adithya <adithya.bellary@gmail.com>
2020-10-06 00:21:39 -0500

    add sentry config

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import { ApolloServer } from 'apollo-server';
 import express from 'express';
 import * as dotenv from 'dotenv';
+import * as Sentry from "@sentry/browser";
+import { Integrations } from "@sentry/tracing";
 // import { ApolloServer } from 'apollo-server-express';
 dotenv.config();
 
@@ -8,12 +10,20 @@ import typeDefs from './schema';
 import resolvers from './resolvers';
 import dataSrc from './datasource';
 
+
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   dataSources: () => ({
     f: new dataSrc()
   })
+});
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  release: "emphasis-education-server@" + process.env.npm_package_version, // To set your release version
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0, // We recommend adjusting this in production
 });
 
 // const app = express();
